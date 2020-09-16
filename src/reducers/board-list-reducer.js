be07fe371582007a219c4fbf1812bb30d6c68fe1
Ian Scott <chesnekov@gmail.com>
2020-09-16 11:24:56 -0700

    add post-list-reducer.js file with crud functionality. Includes ability to upvote/downvote a post via the id
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>

--- src/reducers/board-list-reducer.js
+++ src/reducers/board-list-reducer.js
@@ -2,7 +2,8 @@ import * as c from './actions/ActionTypes.js';
 
 export default (state = {}, action) => {
   const { name, description, id } = action;
-  if (action.type === c.ADD_BOARD){
+  switch (action.type){
+  case c.ADD_BOARD:
     return Object.assign({}, state, {
       [id]: {
         name: name,
@@ -10,11 +11,11 @@ export default (state = {}, action) => {
         id: id
       }
     });
-  } else if (action.type === c.DELETE_BOARD){
+  case c.DELETE_BOARD:
     const newState = {...state};
     delete newState[id];
     return newState;
-  } else {
+  default:
     return state;
   }
 }
---
+++ src/reducers/post-list-reducer.js
@@ -0,0 +1,43 @@
+import * as c from './actions/ActionTypes.js';
+
+export default (state = {}, action) => {
+  const { title, body, date, upvotes, id } = action;
+    switch (action.type) {
+      case c.ADD_POST:
+        return Object.assign({}, state, {
+          [id]: {
+            title: title,
+            body: body,
+            date: date,
+            upvotes: upvotes,
+            id: id
+          }
+        })
+      case c.DELETE_POST:
+        const newState = {...state};
+        delete newState[id];
+        return newState;
+      case c.UPVOTE_POST:
+        const newUpVoteCount = state[id].upvotes +1;
+        return Object.assign({}, state, {
+          [id]: {
+            title: title,
+            body: body,
+            date: date,
+            upvotes: newUpVoteCount,
+            id: id
+          }
+        })
+      case c.DOWNVOTE_POST:
+        const newDownVoteCount = state[id].upvotes -1;
+        return Object.assign({}, state, {
+          [id]: {
+            title: title,
+            body: body,
+            date: date,
+            upvotes: newDownVoteCount,
+            id: id
+          }
+        })  
+    }
+  }
